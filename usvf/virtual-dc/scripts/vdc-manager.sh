80dfe9a44f083091f645c1b4f20a861114276608
Sai Neeraj Chimbili <neeraj.chimbili@juspay.in>
2026-01-24 02:11:23 +0530

    removed cmd debug output

--- usvf/virtual-dc/scripts/vdc-manager.sh
+++ usvf/virtual-dc/scripts/vdc-manager.sh
@@ -23,7 +23,7 @@
 #   ./vdc-manager.sh cleanup-orphans --name prod
 ################################################################################
 
-set -xeuo pipefail
+set -euo pipefail
 
 # Get the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
