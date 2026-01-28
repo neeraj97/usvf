4dab1620a33831239cf83b4d8020ff689faa3834
Sai Neeraj Chimbili <neeraj.chimbili@juspay.in>
2026-01-29 03:10:26 +0530

    FRR syntax fix

--- usvf/kolla-ansible/kolla-node-prepare.sh
+++ usvf/kolla-ansible/kolla-node-prepare.sh
@@ -2,7 +2,12 @@ ssh ubuntu@192.168.10.11 -i config/vdc-dc1/ssh-keys/id_rsa "sudo ip addr add 10.
 ssh ubuntu@192.168.10.12 -i config/vdc-dc1/ssh-keys/id_rsa "sudo ip addr add 10.100.0.254/32 dev lo1"
 ssh ubuntu@192.168.10.13 -i config/vdc-dc1/ssh-keys/id_rsa "sudo ip addr add 10.100.0.254/32 dev lo1"
 
--------------Storage nodes ----------
+-- compute and storage ----
+ssh ubuntu@192.168.10.14 -i config/vdc-dc1/ssh-keys/id_rsa "sudo pvcreate /dev/vdb && sudo vgcreate cinder-volumes /dev/vdb"
+ssh ubuntu@192.168.10.15 -i config/vdc-dc1/ssh-keys/id_rsa "sudo pvcreate /dev/vdb && sudo vgcreate cinder-volumes /dev/vdb"
+
+
+-------------inside each Storage nodes ----------
 sudo modprobe target_core_mod
 sudo modprobe iscsi_target_mod
 sudo modprobe tcm_loop
@@ -16,9 +21,3 @@ sudo apt update
 sudo apt install -y open-iscsi lsscsi sg3-utils multipath-tools
 sudo systemctl enable --now iscsid
 sudo systemctl enable --now multipathd
-
-ssh ubuntu@192.168.10.14 -i config/vdc-dc1/ssh-keys/id_rsa "sudo pvcreate /dev/vdb && sudo vgcreate cinder-volumes /dev/vdb"
-ssh ubuntu@192.168.10.15 -i config/vdc-dc1/ssh-keys/id_rsa "sudo pvcreate /dev/vdb && sudo vgcreate cinder-volumes /dev/vdb"
-
-
-
